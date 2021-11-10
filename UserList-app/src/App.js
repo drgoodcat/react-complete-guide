7a6999bac9eba3382a78cef698e3f5f456895650
drgoodcat <60068941+drgoodcat@users.noreply.github.com>
2021-11-10 14:32:05 +0100

    practice section

--- UserList-app/src/App.js
+++ UserList-app/src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { AddUser } from "./Components/Users/AddUser";
+import { UserList } from "./Components/Users/UsersList";
 
 function App() {
+    const [usersList, setUsersList] = useState([]);
+
+    const addUserHandler = (uName, uAge) => {
+        setUsersList((prevUsersList) => {
+            return [...prevUsersList, { id: Math.random().toString(), name: uName, age: uAge }];
+        });
+    };
+
     return (
         <div>
-            <AddUser></AddUser>
+            <AddUser onAddUser={addUserHandler} />
+            <UserList users={usersList} />
         </div>
     );
 }
--- UserList-app/src/Components/UI/Button.js
+++ UserList-app/src/Components/UI/Button.js
@@ -4,8 +4,8 @@ import classes from "./Button.module.css";
 
 export const Button = (props) => {
     return (
-        <div className={classes.button} type={props.type || "button"} onCLick={props.onCLick}>
+        <button className={classes.button} type={props.type || "button"} onCLick={props.onCLick}>
             {props.children}
-        </div>
+        </button>
     );
 };
